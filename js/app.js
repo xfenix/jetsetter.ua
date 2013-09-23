d8fd4f5c59086e8cd989680325a43551c9ac2c02
xfenix <xfenix@Deniss-MacBook-Air.local>
2013-09-24 03:41:31 +0400

    image mask (barely done), form validation, sidebar header fixes, other small fixes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(function(){
     var HOVER_MENU_TIME = 500,
-        CYCLE_TIMEOUT = 6000;
+        CYCLE_TIMEOUT = 6000,
+        FB_LIKE_WIDTH = 100,
+        FB_LIKE_HEIGHT = 21,
+        
+        isEmpty = function(value) { return /^\s*$/.test(value) };
 
     // hover main menu
     (function(){
@@ -120,6 +124,82 @@ $(function(){
             '.simplemodal-overlay, .popup-registration .cancel',
             function() {
                 $.modal.close();
+            }
+        );
+    })();
+    
+    // validation
+    (function() {
+        var submit = $('.submit'),
+            forms = $('form');
+        
+        submit.on(
+            'click',
+            function() {
+                $(this).closest('form').submit();
+            }
+        );
+        
+        forms.on(
+            'submit',
+            function() {
+                var form = $(this),
+                    valid = true;
+                form.find('.error').hide();
+                form.find('input,textarea').each(function(i) {
+                   var field = $(this),
+                       req = field.attr('required') ? true : false,
+                       pattern = field.attr('pattern'),
+                       fieldType = field.attr('type'),
+                       value = field.val(),
+                       localValid = true;
+                       
+                    if(req) {
+                        if(fieldType == 'email') {
+                            localValid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
+                        }
+                        else if(pattern) {
+                            var re = new RegExp(pattern);
+                            localValid = re.test(value);
+                        }
+                        else if(isEmpty(value)) {
+                            localValid = false;
+                        }
+                    }
+                    
+                    if(!localValid) {
+                        field.closest('.line').find('.error').show();
+                        valid = false;
+                    }
+                });
+                return valid;
+            }
+        );
+    })();
+    
+    // image hovering
+    (function() {
+        var fbLikeUrl = function(url) { return "http://www.facebook.com/plugins/like.php?href=" + url + "&width=" + FB_LIKE_WIDTH + "&height=" + FB_LIKE_HEIGHT + "&colorscheme=light&layout=button_count&action=like&show_faces=false&send=false&appId=272457199470244" },
+            fbIframe = function(url) { return '<iframe src="' + fbLikeUrl(url) + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>'; },
+            ilu = $('.ilu-wrap'),
+            cnt = '.ilu-cnt',
+            mask = 'ilu-mask',
+            show = '.ilu-mask,.ilu-cnt';
+            
+        ilu.each(function() {
+            $(this).append($('<div>').addClass(mask)); 
+        });
+            
+        ilu.hover(
+            function() {
+                var el = $(this),
+                    cntBox = el.find(cnt);
+                if(isEmpty(cntBox.html()))
+                    cntBox.html(fbIframe(encodeURI(cntBox.data('fb'))));
+                el.find(show).show();
+            },
+            function() {
+                $(this).find(show).hide();
             }
         );
     })();
